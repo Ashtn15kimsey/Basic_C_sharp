fa3fc61724907578673892b01e69ff4225ad86fc
ashtn15kimsey <ashtn15@gmail.com>
2021-05-19 12:13:53 -0700

    updates

---
+++ Helloworld!/Helloworld!/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Welcome to Acme Accunting Systems");
+        Console.WriteLine("Remember, we're 'Accounting' on you!");
+        Console.ReadLine();
+        }
+    }
+
