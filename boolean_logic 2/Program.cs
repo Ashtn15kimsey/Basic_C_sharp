f1cc1ed31a5fca9f16cb0b0ae380f1ea76a15d09
ashtn15kimsey <ashtn15@gmail.com>
2021-05-20 10:46:29 -0700

    update

---
+++ boolean_logic 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boolean_logic_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AND operator
+            //Console.WriteLine(true && false);
+            //Console.ReadLine();
+
+            //OR operator 
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+            //Console.ReadLine();
+
+            //== operator
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == true);
+            //Console.ReadLine();
+
+            // Does not == operator
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+            //Console.ReadLine();
+
+            //x OR operator
+            Console.WriteLine(true ^ true);
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(false ^ false);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
