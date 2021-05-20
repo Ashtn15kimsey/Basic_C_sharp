3a37fb303b57a9f61b4efb34d776393a160f5f59
ashtn15kimsey <ashtn15@gmail.com>
2021-05-19 20:07:57 -0700

    update

--- Boolean_Logic/Program.cs
+++ Boolean_Logic/Program.cs
@@ -10,30 +10,62 @@ namespace Boolean_Logic
     {
         static void Main(string[] args)
         {
-            // AND argument '&&'
+            //1). // AND argument '&&'
             //int packageWidth = 5;
             //int packageHeight = 15;
             //bool tooBig = (packageHeight > 20 && packageWidth > 20);
             //Console.WriteLine(tooBig);
             //Console.ReadLine();
 
-            // OR arguement symbol '||'
+            //2) // OR arguement symbol '||'
             //int packWwidth = 9;
             //int packageHeight = 10;
             //bool tooBig = (packageHeight > 7 || packWwidth > 7);
             //Console.WriteLine(tooBig);
             //Console.ReadLine();
 
-            // More than one AND in expression argument
+            //3) // More than one AND in expression argument
 
-            string firstName = "Ash";
-            string lastName = "kimsey";
-            string socialSecurityNumber = "222-22-2222";
+            //string firstName = "Ash";
+            //string lastName = "kimsey";
+            //string socialSecurityNumber = "222-22-2222";
 
-            bool isAuthorized = (firstName == "Ash" && lastName == "kimsey" && socialSecurityNumber == "222-22-2222");
-            Console.WriteLine(isAuthorized);
-            Console.ReadLine();
+            //bool isAuthorized = (firstName == "Ash" && lastName == "kimsey" && socialSecurityNumber == "222-22-2222");
+            //Console.WriteLine(isAuthorized);
+            //Console.ReadLine();
+            //“True or false ?: Is this person’s first name ‘Jesse’ and his last name ‘Johnson’ and is his social security number ‘111 - 11 - 1111’?”
+
+
+
+
+
+            //4)//// OR operator in expression argument
+            //int banckAccountBalance = 100;
+            //bool isInRockBand = false;
+            //string = "ash";
+            //bool isReallyCool = (banckAccountBalance > 100000 || isInRockBand || name == "Ash");
+            //Console.WriteLine(isReallyCool);
+            //Console.ReadLine();
+
+            //The above expression asks the question, “True or false ?: Does this person have over a hundred thousand dollars in his bank account or is he in a rock band or is his name Jesse ?”
+
+            //5) //Both operators comined and used '&&' '||'
+            // This code asked the question, "True or False? is this true AND either this or this true"?
+            //int num1 = 7;
+            //int num2 = 12;
+            //int num3 = 23;
+            //int num4 = 14;
+            //int num5 = 8;
+            //int num6 = 5;
+
+            //bool result = (num1 > num2 && (num3 == num4 || num5 > num6));
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+
+
+
         }
+
 
 
     }
