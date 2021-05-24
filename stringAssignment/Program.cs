4e7b65a5662e3e6068b5dce3b111fc062e681580
ashtn15kimsey <ashtn15@gmail.com>
2021-05-23 21:49:11 -0700

    update

---
+++ stringAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string conc. method
+            string fname = "Ash";
+            string lname = "kimsey";
+            string MiddleName = "michael";
+            string name = string.Concat(fname, lname, MiddleName);
+
+            // converting string to uppercase
+            string coolName = "Ash";
+            string upperCase = coolName.ToUpper();
+
+            // Creatin a StringBuilder
+            StringBuilder sb = new StringBuilder("Hello Ash");
+
+
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+             
+        }
+    }
+}
