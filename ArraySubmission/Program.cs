664270d3c680c3dbd175bd26cf0dec8c24fca8d1
ashtn15kimsey <ashtn15@gmail.com>
2021-05-23 22:59:15 -0700

    update

---
+++ ArraySubmission/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ArraySubmission
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] nameArray = new string[] { "apple", "PineApples", "pears" };
+            Console.WriteLine("Choose fruit");
+            int index = Convert.ToInt32(Console.ReadLine());
+            if (index >= 0 && index <=)
+            {
+                Console.WriteLine(nameArray[index]);
+                Console.ReadLine();
+            }
+
+            int[] numArray = new int[] { 2, 4., 6, 56, 35 };
+            Console.WriteLine("choose a number");
+            int index2 = Convert.ToInt32(Console.ReadLine());
+            if (index2 >= 0 && index2 <= 6)
+            {
+                Console.WriteLine(numArray[index2]);
+            }
+            else
+            {
+                Console.WriteLine("sorry that number does not exist");
+            }
+            List<string> intList = new List<string>();
+            intList.Add("My Name is Ash");
+            intList.Add("I'm 23 years old");
+            intList.Remove("The sky is blue today");
+            Console.WriteLine("select and index");
+
+
+
+
+                
+
+
+
+        }
+    }
+}
---
+++ Arrays/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            List<string > intList = new List<string >();
+            intList.Add("Hello");
+            intList.Add("Ash");
+            intList.Remove("Ash");
+
+            Console.WriteLine(intList[0]);
+            Console.ReadLine();
+                
+
+            
+
+
+
+            // exaple 1
+            //int[] numArray = new int[5];
+            //numArray[0] = 5;
+            //numArray[1] = 2;
+            //numArray[2] = 10;
+            //numArray[3] = 200;
+            //numArray[4] = 500;
+
+            ////exaple 2
+            //int[] numArray1 = new int[] { 5, 2, 10, 200, 500 };
+
+            ////example 3
+            //int[] numArray2 = { 5, 2, 10, 200, 500, 600, 2500 };
+            //numArray2[5] = 700;
+
+            
+
+            Console.WriteLine();
+            Console.ReadLine();
+               
+
+
+        }
+    }
+}
