372269acc81b493aefa555da40c5b28c96628c2f
ashtn15kimsey <ashtn15@gmail.com>
2021-05-25 13:45:45 -0700

    update

---
+++ AppAssignment2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace AppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fruits = { "apples\t", "pineApples\t", "pears\t" };
+            Console.WriteLine("Add favortie fruit");
+            string addFruit = Console.ReadLine();
+            foreach (string frutis in fruits) ;
+            {
+                string NewFruit = fruits + addFruit;
+                Console.WriteLine(newFruit);
+                
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
