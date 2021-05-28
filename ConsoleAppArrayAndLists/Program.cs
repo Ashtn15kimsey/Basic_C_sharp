aeeec51c404e86f6d301402bce967e54dff527b6
ashtn15kimsey <ashtn15@gmail.com>
2021-05-27 20:28:14 -0700

    update

--- ConsoleAppArrayAndLists/Program.cs
+++ ConsoleAppArrayAndLists/Program.cs
@@ -11,71 +11,86 @@ namespace ConsoleAppArrayAndLists
 
             //string[] names = { "Ash\t", "josh\t", "Jerry\t", "Peter\t" };
 
-            Console.WriteLine("Add your name:\n");
-            string AddName = Console.ReadLine();
-            foreach (string name in names)
+            //        Console.WriteLine("Add your name:\n");
+            //        string AddName = Console.ReadLine();
+            //        foreach (string name in names)
+            //        {
+            //            string NewName = name + AddName;
+            //            Console.WriteLine(NewName);
+            //        }
+            //        Console.ReadLine();
+
+            //        //infninite loop
+            //        for (int a = 0; a < 50; a--)
+            //        {
+            //            Console.WriteLine("ininite loop: {0}", 0);
+            //    }
+            //    Console.ReadLine();
+
+            //    //loop comparassion
+            //    int num;
+            //    num = 0;
+            //    while (num < 40)
+            //    {
+            //        Console.WriteLine(num);
+            //        num++;
+            //    }
+            //    int num2;
+            //    num2 = 0;
+            //    while (num2 <= 20)
+            //    {
+            //        Console.WriteLine(num2);
+            //        num2++;
+            //}
+            //     //List with Unique strings
+            //     List<string>  stringList = new List<string>();
+            //    stringList.Add("samantha");
+            //     stringList.Add("aaron");
+            //     bool Guessed = false;
+
+            //do
+            //{
+            //     Console.WriteLine("select name");
+            //    string AddName = Console.Readline();
+
+            //     if (AddName == "aaron")
+            //     {
+            //        Console.WriteLine("you have selected: \t" + stringList[0]);
+            //                break;
+
+            //         }
+
+            //    if (AddName == "samantha")
+            //    {
+            //        Console.WriteLine("you have selected: \t + stringList[1]);
+            //        break;
+            //    }
+
+            //    else
+            //    {
+            //            Console.WriteLine("not on the list");
+            //    }
+
+            //     {
+            //                while (!Guessed) ;
+            //                Console.ReadLine();
+
+            //     }
+
+            // List of strings with two identitcal strings 
+            List<string> name = new List<string>() { "Ash", "Ash" };
+            List<string> display = new List<string>();
+
+            foreach (string name in name)
             {
-                string NewName = name + AddName;
-                Console.WriteLine(NewName);
+                if (name == "Ash")
+                {
+                    display.Add(name);
+                }
             }
+            Console.WriteLine("Ash:" + display.Count);
             Console.ReadLine();
 
-            //infninite loop
-            for (int a = 0; a < 50; a--)
-            {
-                Console.WriteLine("ininite loop: {0}", 0);
-        }
-        Console.ReadLine();
-
-        //loop comparassion
-        int num;
-        num = 0;
-        while (num < 40)
-        {
-            Console.WriteLine(num);
-            num++;
-        }
-        int num2;
-        num2 = 0;
-        while (num2 <= 20)
-        {
-            Console.WriteLine(num2);
-            num2++;
-    }
-         //List with Unique strings
-         List<string>  stringList = new List<string>();
-        stringList.Add("samantha");
-         stringList.Add("aaron");
-         bool Guessed = false;
-
-    do
-    {
-         Console.WriteLine("select name");
-        string AddName = Console.Readline();
-
-         if (AddName == "aaron")
-         {
-            Console.WriteLine("you have selected: \t" + stringList[0]);
-                    break;
-        
-             }
-
-        if (AddName == "samantha")
-        {
-            Console.WriteLine("you have selected: \t + stringList[1]);
-            break;
-        }
-
-        else
-        {
-                Console.WriteLine("not on the list");
-        }
-
-         {
-                    while (!Guessed) ;
-                    Console.ReadLine();
-
-         }
             
 
 
