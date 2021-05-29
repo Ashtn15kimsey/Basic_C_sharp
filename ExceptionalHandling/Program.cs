6062e9bfcc292bedf0f071959ca5d8cc61ab8f12
ashtn15kimsey <ashtn15@gmail.com>
2021-05-28 18:44:36 -0700

    update

---
+++ ExceptionalHandling/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ExceptionalHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> numbers = new List<int>();
+                numbers.Add(6);
+                numbers.Add(10);
+                numbers.Add(14);
+                Console.WriteLine("choose a number");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                foreach (int number in numbers)
+                {
+                    int numberThree = number / numberOne;
+                    Console.WriteLine(number + "divide by" + numberOne + "equals:" + numberThree);
+                }
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("please type a whole number");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("please dont divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+    }
+}
