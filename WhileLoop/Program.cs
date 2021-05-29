a7e392716064c3081e828caf6042f39d0dd144f8
ashtn15kimsey <ashtn15@gmail.com>
2021-05-28 18:00:54 -0700

    update

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("guess a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool IsGuessed = number == 14;
+
+            do
+            {
+                switch (number)
+                {
+
+                    case 13:
+                        Console.WriteLine("you gussed 13. try again");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 6:
+                        Console.WriteLine("you guessed 6. try again");
+                        Console.WriteLine("guess a numner");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 45:
+                        Console.WriteLine("you gussed 45. try agin");
+                        Console.WriteLine("guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 14:
+                        Console.WriteLine("you gussed 14. That is is correct");
+                        IsGuessed = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("you are wrong");
+                        Console.WriteLine("guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+
+
+
+
+
+
+                }
+
+            }
+
+            while (!IsGuessed);
+
+            Console.ReadLine();
+        }
+    }
+}
--- WhileLoop/Properties/AssemblyInfo.cs
+++ WhileLoop/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AppAssignment")]
+[assembly: AssemblyTitle("WhileLoop")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AppAssignment")]
+[assembly: AssemblyProduct("WhileLoop")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("94abb415-0567-4568-b979-2dbd28543a45")]
+[assembly: Guid("cf6e2021-dbb9-4aff-b23d-c08f793cf028")]
 
 // Version information for an assembly consists of the following four values:
 //
