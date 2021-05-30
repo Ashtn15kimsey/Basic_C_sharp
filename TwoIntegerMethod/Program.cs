f4fe6be0f5b8fd334d511f22e2a3586f519d4db5
ashtn15kimsey <ashtn15@gmail.com>
2021-05-29 19:02:30 -0700

    update

---
+++ TwoIntegerMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoIntegerMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter a second number");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            IntigerTwo myMath = new IntigerTwo();
+            if (y == 0)
+            {
+                int result = myMath.Subtraction(x);
+                Console.WriteLine(result);
+              
+            }
+            else
+            {
+                int result = myMath.Subtraction(x, y);
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
+        }
+    }
+}
