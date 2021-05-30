9df8808d20bc7ceb468c0c6cc964a4a2658e3e99
ashtn15kimsey <ashtn15@gmail.com>
2021-05-29 19:18:28 -0700

    update

---
+++ TwoIntiger2/Multipilcation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoIntiger2
+{
+    class Multipilcation
+    {
+        public void Times(int num1, int num2)
+        {
+            num1 = num1 * 18;
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ TwoIntiger2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoIntiger2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Multipilcation a = new Multipilcation();
+
+            a.Times(23, 98);
+            a.Times(num1: 8, num2: 14);
+            Console.ReadLine();
+        }
+    }
+}
