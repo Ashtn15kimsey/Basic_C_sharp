884f64a12cc0ed396f0ca2c80dd709f418615a66
ashtn15kimsey <ashtn15@gmail.com>
2021-05-30 19:39:59 -0700

    update

---
+++ MethodAndObjectAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAndObjectAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee();
+            person.FirstName = "sample";
+            person.LastName = "student";
+
+            person.SayName();
+
+            Console.ReadLine();
+
+        }
+    }
+}
