23fa78baf7c445d13ab7ad527f4248542e9dd10a
ashtn15kimsey <ashtn15@gmail.com>
2021-06-04 11:35:17 -0700

    Update

---
+++ DateAnd Time Assignments/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateAnd_Time_Assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- Input Assignment/Program.cs
+++ Input Assignment/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Input_Assignment
 {
@@ -10,6 +11,13 @@ namespace Input_Assignment
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter number here!");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number);
+            using  (StreamWriter file = new StreamWriter(@"c:\User\Documents\IO.txt", true))
+            {
+                file.WriteLine(number);
+            }
         }
     }
 }
