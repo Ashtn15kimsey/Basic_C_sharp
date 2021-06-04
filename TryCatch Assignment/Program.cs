adcd0d57bf3603b86ca61f2adc9d84fa31656eb0
ashtn15kimsey <ashtn15@gmail.com>
2021-06-04 14:19:20 -0700

    update

---
+++ TryCatch Assignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ageText;
+            int age;
+            int result = 0;
+
+            Console.WriteLine("Please enter your age:");
+            ageText = Console.ReadLine();
+
+            try
+            {
+                age = int.Parse(ageText);
+                Console.WriteLine("your are {age*12}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"The age you entered, {ageText}, is not valid");
+                result = 1;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Unexpexted error: {exception.Message}");
+                result = 1;
+            }
+            Console.ReadLine();
+                
+        }
+    }
+}
+        
+            
+
+           
+                
